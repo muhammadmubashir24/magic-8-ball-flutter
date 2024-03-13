7be7e647e174a8634217aa83ad82c20f0b0c0ee3
muhammadmubashir24 <muhammad.mubashir2498@gmail.com>
2024-03-13 16:03:42 +0500

    Complete 8 Ball Project Task

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,60 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            centerTitle: true,
+            title: const Text(
+              'Ask Me Anything',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          body: const BallPage(),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
     );
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  void changeBallFace() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeBallFace();
+                // print('I got clicked');
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
